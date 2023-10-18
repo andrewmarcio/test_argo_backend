cdcc7a662ac78c5b79ea5fa03e3a6d736dd92365
andrewmarcio <andrewmarcio21@gmail.com>
2023-10-18 01:59:50 -0300

    feat: Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Pull branch application"
+git pull origin main
+
+# echo "Execute docker compose down"
+# docker compose down
+
+# echo "Start docker container"
+# docker compose up -d
+
+echo "Install composer packages"
+docker run -it todo_api:1.0.0 /bin/sh -c cd /var/www/todo_api && composer install
+
+echo "Execute migrate database"
+docker run -it todo_api:1.0.0 /bin/sh -c cd /var/www/todo_api && php artisan migrate
+
+echo "Execute migrate database"
+docker run -it todo_api:1.0.0 /bin/sh -c cd /var/www/todo_api && php artisan optimize:clear
